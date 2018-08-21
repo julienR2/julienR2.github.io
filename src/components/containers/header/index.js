cec0c0929ba38ae6a6a340ef14073ea29895ea97
julienr2 <julien.rougeron@gmail.com>
2018-08-21 13:58:33 +0200

    adapt header for mobile

--- src/components/containers/header/index.js
+++ src/components/containers/header/index.js
@@ -1,10 +1,8 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import {
   Container,
-  Collapse,
   Navbar,
-  NavbarToggler,
   Nav,
   NavItem,
   NavLink,
@@ -12,64 +10,50 @@ import {
 
 import './styles.scss';
 
-export default class Header extends Component {
+export default class Header extends PureComponent {
   static propTypes = {
     scrolled: PropTypes.bool,
   }
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isOpen: false,
-    };
-  }
-
-  toggle = () => this.setState(prevState => ({ isOpen: !prevState.isOpen }));
-
   render() {
-    const { isOpen } = this.state;
     const { scrolled } = this.props;
 
     return (
       <div>
         <Navbar expand="md" fixed="top" className={scrolled ? 'scrolled' : ''}>
           <Container>
-            <NavbarToggler onClick={this.toggle} />
-            <Collapse isOpen={isOpen} navbar>
-              <Nav navbar>
-                <NavItem>
-                  <NavLink href="#services">
-                    What I Do
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink href="#featured">
-                    Featured
-                  </NavLink>
-                </NavItem>
-                <NavItem className="logo">
-                  <NavLink href="#home">
-                    <h1>
-                      J
-                      <span>
-                        R
-                      </span>
-                    </h1>
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink href="#portfolio">
-                    My Works
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink href="#about-me">
-                    About Me
-                  </NavLink>
-                </NavItem>
-              </Nav>
-            </Collapse>
+            <Nav navbar>
+              <NavItem>
+                <NavLink href="#services">
+                  What I Do
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#featured">
+                  Featured
+                </NavLink>
+              </NavItem>
+              <NavItem className="logo">
+                <NavLink href="#home">
+                  <h1>
+                    J
+                    <span>
+                      R
+                    </span>
+                  </h1>
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#portfolio">
+                  My Works
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#about-me">
+                  About Me
+                </NavLink>
+              </NavItem>
+            </Nav>
           </Container>
         </Navbar>
       </div>
